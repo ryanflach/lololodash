6b14f4ac34c51a1c2e50373c1052dcfe89bb607e
Ryan Flach <ryanflach@users.noreply.github.com>
2016-09-26 11:59:49 -0600

    Complete some/every exercise

---
+++ everyone-is-min.js
@@ -0,0 +1,26 @@
+// include the Lo-Dash library
+var _ = require("lodash");
+
+var someWarmth = function(collection){
+  var result = {
+    hot: [],
+    warm: []
+  };
+
+  function checkTemp(temp) {
+    return temp > 19;
+  }
+
+  _.forEach(collection, function(town, townName){
+    if (_.every(town, checkTemp)) {
+      result.hot.push(townName);
+    } elseif (_.some(town, checkTemp)) {
+      result.warm.push(townName);
+    }
+  });
+
+  return result;
+};
+
+// export the function as a nodejs module
+module.exports = someWarmth;
